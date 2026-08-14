b07343cc8830748c2536c9cde82ceb92d511a080
Shihab177 <shihab7777h@gmail.com>
2026-08-14 12:40:32 +0600

    add PopularCakes component

---
+++ src/components/public/home/PopularCakes.tsx
@@ -0,0 +1,156 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ActivityIndicator,
+} from "react-native";
+import { useRouter } from "expo-router";
+import { truncateWords } from "@/utils/textUtils";
+type TPopularCakes = {
+  _id: string;
+  title: string;
+  description: string;
+  categoryId: string;
+  images: string[];
+  ingredients: string[];
+  reviewId?: string | null;
+  price: number;
+  qty: number;
+};
+const PopularCakes = () => {
+  const [popularCakes, setPopularCakes] = useState<TPopularCakes[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const router = useRouter();
+  const handleCardPress = (id: string) => {
+    router.push(`/cake/${id}`);
+  };
+  useEffect(() => {
+    const controller = new AbortController();
+    const getPopularCakes = async () => {
+      try {
+        const response = await axios.get(
+          "http://192.168.0.100:3160/api/v1/cakes",
+          {
+            signal: controller.signal,
+          },
+        );
+        setPopularCakes(response.data?.data || []);
+      } catch (error) {
+        if (!axios.isCancel(error)) {
+          console.error("API Error:", error);
+        }
+      } finally {
+        if (!controller.signal.aborted) {
+          setLoading(false);
+        }
+      }
+    };
+    getPopularCakes();
+    return () => controller.abort();
+  }, []);
+  if (loading) {
+    return (
+      <View style={styles.loaderContainer}>
+        <ActivityIndicator size="small" color="#301F1F" />
+      </View>
+    );
+  }
+  if (popularCakes.length < 1) {
+    return (
+      <View style={styles.loaderContainer}>
+        <Text>Popular cake not found</Text>
+      </View>
+    );
+  }
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.sectionText}>POPULAR CAKES</Text>
+      </View>
+      <FlatList<TPopularCakes>
+        data={popularCakes}
+        keyExtractor={(item) => item._id}
+        numColumns={2}
+        scrollEnabled={false}
+        columnWrapperStyle={styles.columnWrapper}
+        contentContainerStyle={styles.listContainer}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => handleCardPress(item._id)}
+            activeOpacity={0.8}
+            style={styles.card}
+          >
+            <Image
+              source={{ uri: item?.images?.[0] }}
+              style={styles.cardImage}
+              resizeMode="cover"
+            />
+            <Text style={styles.cardTitle} numberOfLines={1}>
+              {item.title}
+            </Text>
+            <Text style={styles.cardDescription} numberOfLines={2}>
+             {truncateWords(item.description, 10)}
+            </Text>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
+};
+
+export default PopularCakes;
+
+const styles = StyleSheet.create({
+  loaderContainer: {
+    paddingVertical: 30,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  container: {
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  titleContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+  sectionText: {
+    fontSize: 12,
+    color: "#301F1F",
+    fontWeight: "500",
+    letterSpacing: 0.5,
+  },
+  columnWrapper: {
+    justifyContent: "space-between",
+  },
+  listContainer: {
+    gap: 16,
+  },
+  card: {
+    width: "48%",
+  },
+  cardImage: {
+    width: "100%",
+    height: 121,
+    borderRadius: 8,
+  },
+  cardTitle: {
+    fontSize: 16,
+    fontWeight: "500",
+    color: "#301F1F",
+    marginTop: 8,
+  },
+  cardDescription: {
+    fontSize: 12,
+    color: "#828282",
+    fontWeight: "400",
+    marginTop: 4,
+    lineHeight: 16,
+  },
+});

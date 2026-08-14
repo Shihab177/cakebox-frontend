aa4fa2b8e2c91f8186ceb2ca24ce494564c1ade0
Shihab177 <shihab7777h@gmail.com>
2026-08-14 12:43:28 +0600

    add bottom tabs

---
+++ src/app/(public)/(tabs)/_layout.tsx
@@ -0,0 +1,76 @@
+import { Tabs } from 'expo-router';
+import { View, StyleSheet, Platform } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Feather, Ionicons } from '@expo/vector-icons';
+
+export default function TabLayout() {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <Tabs
+      initialRouteName="index"
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: '#FF6B00',
+        tabBarInactiveTintColor: '#8E8E93',
+        tabBarStyle: {
+          backgroundColor: '#FFFFFF',
+          borderTopWidth: 0,
+          elevation: 10,
+          shadowColor: '#000',
+          shadowOffset: { width: 0, height: -2 },
+          shadowOpacity: 0.05,
+          shadowRadius: 10,
+          // ডায়নামিক হাইট ও প্যাডিং সেটআপ
+          height: Platform.OS === 'ios' ? 60 + insets.bottom : 60 + insets.bottom,
+          paddingBottom: insets.bottom > 0 ? insets.bottom : 10,
+          paddingTop: 10,
+        },
+      }}
+    >
+     
+
+      <Tabs.Screen
+        name="cart"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Feather name="shopping-bag" size={22} color={color} />
+          ),
+        }}
+      />
+       <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ color }) => (
+          
+              <Ionicons
+                name="grid-outline"
+                size={22}
+                color={ color}
+              />
+          
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Feather name="user" size={22} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
+
+const styles = StyleSheet.create({
+  activeTabBg: {
+    backgroundColor: '#FFF0E6',
+    padding: 10,
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
